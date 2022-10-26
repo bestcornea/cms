40e473c4655087dd7195e624af0fc8107556dea4
joostdecock <joost@joost.at>
2022-10-26 18:08:31 +0200

    chore: Updated Sanity config

--- schemas/page.js
+++ schemas/page.js
@@ -4,22 +4,42 @@ export default {
   type: 'document',
   fields: [
     {
-      name: 'title',
-      title: 'Title',
-      type: 'string',
-    },
-    {
       name: 'slug',
       title: 'Slug',
       type: 'slug',
       options: {
-        source: 'title',
+        source: 'titleen',
         maxLength: 96,
       },
     },
     {
-      name: 'body',
-      title: 'Body',
+      name: 'titleen',
+      title: 'English Title',
+      type: 'string',
+    },
+    {
+      name: 'bodyen',
+      title: 'English Body',
+      type: 'markdown',
+    },
+    {
+      name: 'titlenl',
+      title: 'Dutch Title',
+      type: 'string',
+    },
+    {
+      name: 'bodynl',
+      title: 'Dutch Body',
+      type: 'markdown',
+    },
+    {
+      name: 'titlefr',
+      title: 'French Title',
+      type: 'string',
+    },
+    {
+      name: 'bodyfr',
+      title: 'French Body',
       type: 'markdown',
     },
   ],
